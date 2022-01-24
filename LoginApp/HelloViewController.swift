069334ffb4ab102388b75a78a183cd9577381198
Nikolay Trofimov <nikktro@gmail.com>
2022-01-25 00:15:14 +0300

    add new HelloViewController and class

---
+++ LoginApp/HelloViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HelloViewController.swift
+//  LoginApp
+//
+//  Created by Nikolay Trofimov on 25.01.2022.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
