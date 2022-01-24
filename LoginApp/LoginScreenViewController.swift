a0ee33a0352316aedd4885da4a8723d66d402de3
Nikolay Trofimov <nikktro@gmail.com>
2022-01-24 23:57:53 +0300

    LoginScreenViewController outlets

---
+++ LoginApp/LoginScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Nikolay Trofimov on 24.01.2022.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func logInButtonPressed() {
+    }
+    
+    
+    @IBAction func forgotNameButtonPressed() {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+    }
+    
+    
+}
+
