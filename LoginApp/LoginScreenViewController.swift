b19e6800e55ecb49e05c93dc119c64e2011a6932
Nikolay Trofimov <nikktro@gmail.com>
2022-01-25 00:13:18 +0300

    LoginScreenVC add logic for forgotten user data

--- LoginApp/LoginScreenViewController.swift
+++ LoginApp/LoginScreenViewController.swift
@@ -12,22 +12,28 @@ class LoginScreenViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    @IBAction func logInButtonPressed() {
-    }
-    
-    
     @IBAction func forgotNameButtonPressed() {
+        showNotification(title: "OOPS!!", message: "You username is 'User'")
     }
     
     @IBAction func forgotPasswordButtonPressed() {
+        showNotification(title: "OOPS!!", message: "You password is 'Password'")
     }
-    
     
 }
 
+extension LoginScreenViewController {
+    
+    private func showNotification(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+    
+}
