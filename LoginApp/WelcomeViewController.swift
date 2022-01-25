75514fee830e003187bc3b79bc4fcd2ee776c898
Nikolay Trofimov <nikktro@gmail.com>
2022-01-26 01:02:02 +0300

    add gradient background

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -16,9 +16,24 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setGradientBackground()
+        
         if let userName = userName {
             helloLabel.text = "Hello, \(userName)!"
         }
         
     }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 155.0/255.0, blue: 155.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 10.0/255.0, green: 125.0/255.0, blue: 175.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
 }
