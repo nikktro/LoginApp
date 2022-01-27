5ea7a5ee26464b56fdf1ca24e45628cfb2db8772
Nikolay Trofimov <nikktro@gmail.com>
2022-01-27 23:57:39 +0300

    recommendations fix

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -12,6 +12,9 @@ class LoginViewController: UIViewController {
     @IBOutlet var usernameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
+    private var user = "User"
+    private var password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         usernameTF.delegate = self
@@ -24,66 +27,60 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonPressed() {
-        if loginSuccess() {
-            performSegue(withIdentifier: "loginSuccess", sender: nil)
+        if !loginSuccess() {
+            showNotification(
+                title: "Invalid login or password",
+                message: "Please enter correct login and password") { _ in
+                    self.passwordTF.text = ""
+                }
         }
     }
     
     @IBAction func forgotNameButtonPressed() {
-        showNotification(title: "Oops!!", message: "You username is 'User'")
+        showNotification(title: "Oops!!", message: "You username is '\(user)'")
     }
     
     @IBAction func forgotPasswordButtonPressed() {
-        showNotification(title: "Oops!!", message: "You password is 'Password'")
+        showNotification(title: "Oops!!", message: "You password is '\(password)'")
     }
     
-    @IBAction func unwindToLoginVC(_ unwindSegue: UIStoryboardSegue) {
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
         usernameTF.text = ""
         passwordTF.text = ""
     }
     
 }
 
+// MARK: - Private Methods
+extension LoginViewController {
+    private func loginSuccess() -> Bool {
+        usernameTF.text == user && passwordTF.text == password
+    }
+}
+
+// MARK: - Alert Controller
+extension LoginViewController {
+    private func showNotification(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: handler)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
 extension LoginViewController: UITextFieldDelegate {
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == usernameTF {
-            textField.resignFirstResponder()
             passwordTF.becomeFirstResponder()
         } else if textField == passwordTF {
-            textField.resignFirstResponder()
             loginButtonPressed()
         }
         return true
     }
-    
-    private func showNotification(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
-        
-        let alertController = UIAlertController(title: title,
-                                                message: message,
-                                                preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "OK", style: .default, handler: handler)
-        alertController.addAction(action)
-        present(alertController, animated: true)
-    }
-    
-    private func loginSuccess() -> Bool {
-        if usernameTF.text == "User" && passwordTF.text == "Password" {
-            return true
-        }
-        
-        showNotification(
-            title: "Invalid login or password",
-            message: "Please enter correct login and password") { _ in
-                self.passwordTF.text = ""
-            }
-        
-        return false
-    }
-    
 }
