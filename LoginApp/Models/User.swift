46787b5f0655cffb074953d23cf298eb6d88e446
Nikolay Trofimov <nikktro@gmail.com>
2022-02-03 04:28:54 +0300

    update Model

--- LoginApp/Models/User.swift
+++ LoginApp/Models/User.swift
@@ -11,6 +11,19 @@ struct User {
     let user: String
     let password: String
     let person: Person
+    
+    static func getUser() -> User {
+        User(user: "User",
+             password: "Password",
+             person: Person(name: "Nikolay",
+                            surname: "Trofimov",
+                            about: "Hi there! My name is Nikolay!\n" +
+                            "I was born November 19, 1985 in Moscow.\n" +
+                            "After graduating from school I entered the University of Communications and Informatics. " +
+                            "Currently work for a Swedish 5G telecommunications company. " +
+                            "I'm interested in mobile development with Swift.\n",
+                            photo: "Nikk"))
+    }
 }
 
 struct Person {
